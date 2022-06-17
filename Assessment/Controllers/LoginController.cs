124fa4a86f0d29e69a2d1ce5d53ccad5a87fbb31
aliszamri <106647776+aliszamri@users.noreply.github.com>
2022-06-17 14:37:54 +0800

    Upload

---
+++ Assessment/Controllers/LoginController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assessment.Controllers
+{
+    using Assessment.Models;
+    using System.Web.Security;
+
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Login usermodel, string ReturnUrl)
+        {
+                if (usermodel.Username != "alisalia2000@gmail.com")
+                {
+                    ModelState.AddModelError("Username", "Username does not belong to an account.");
+                }
+                else if (usermodel.Password != "Aliszamri178")
+                {
+                    ModelState.AddModelError("Password", "Password is incorrect.");
+                }
+                else
+                {
+                    FormsAuthentication.SetAuthCookie(usermodel.Username, false);
+                    Session["Username"] = usermodel.Username.ToString();
+
+                    if (ReturnUrl != null)
+                    {
+                        return Redirect(ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+
+            return View();
+        }
+        [Authorize]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session["Username"] = null;
+            return RedirectToAction("Index", "Login");
+
+        }
+    }
+}
---
+++ Assessment/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assessment.Models
+{
+    public class Login
+    {
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Please enter your username.")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Please enter your password.")]
+        public string Password { get; set; }
+    }
+}
